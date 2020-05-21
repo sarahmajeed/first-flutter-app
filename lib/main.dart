0c0339416b949c132ca39fa5f6a41ef87c68c5ff
Sarahmdawood <sarahmajeed368@gmail.com>
2020-05-21 21:28:23 +0500

    Adding Scaffold widget instead of Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart'; //built in widgets
 
-void main(){
-  runApp(MyApp()); //runApp is a function provided in material.dart . It takes a widget and calls the build method to draw
-}
+void main() => runApp(MyApp()); //runApp is a function provided in material.dart . It takes a widget and calls the build method to draw
+
 
 class MyApp extends StatelessWidget {
-  @override //App works without this but it is a decorator provided by dart. It makes code clear clean. Says that we are overriding the method build.
-  Widget build(BuildContext context) { //context is a special object of type BuildContext
-      return MaterialApp(home: Text('Hello!') ); //home: is a named argument. Both MaterialApp and Text are dart classes
+  @override 
+  Widget build(BuildContext context) { 
+    
+      return MaterialApp(home: Scaffold( 
+        appBar: AppBar(title:Text("My First App")),
+        body: Text("This is my default text"),), 
+         );
   }
 }
