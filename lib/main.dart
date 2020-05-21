ab742df7e64febca67f802cd4ea8762d4850cd95
Sarahmdawood <sarahmajeed368@gmail.com>
2020-05-21 21:13:57 +0500

    lib/Adding comments for clarity

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ void main(){
 }
 
 class MyApp extends StatelessWidget {
+  @override //App works without this but it is a decorator provided by dart. It makes code clear clean. Says that we are overriding the method build.
   Widget build(BuildContext context) { //context is a special object of type BuildContext
-      return MaterialApp(home: Text('Hello!') ); //home: is a named argument
+      return MaterialApp(home: Text('Hello!') ); //home: is a named argument. Both MaterialApp and Text are dart classes
   }
 }
